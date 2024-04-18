4d81cdc871112ca7d2f5810397002eb30908b4af
Vicky11117713 <159543210+Vicky11117713@users.noreply.github.com>
2024-04-18 16:48:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function showProducts() {
+    document.querySelector(".hero").style.display = "none";
+    document.querySelector(".products").style.display = "flex";
+  }
+  
+  function showHome() {
+    document.querySelector(".hero").style.display = "block";
+    document.querySelector(".products").style.display = "none";
+  }
+  
